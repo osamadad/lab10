553d7407d81a2cee1a1c2921d71b3a45108b6b27
osama dad <osamadad768@gmail.com>
2025-11-24 10:15:01 +0300

    initial commit

---
+++ src/main/java/com/tuwaiq/lab10_jobseeking/Lab10JobSeekingApplication.java
@@ -0,0 +1,13 @@
+package com.tuwaiq.lab10_jobseeking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab10JobSeekingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab10JobSeekingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuwaiq/lab10_jobseeking/Lab10JobSeekingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuwaiq.lab10_jobseeking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab10JobSeekingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
